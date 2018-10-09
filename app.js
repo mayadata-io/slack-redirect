6895b5700c9bcff3501ff2f402c9c2385dfae62a
pasindu94 <33244732+pasindu94@users.noreply.github.com>
2018-10-09 08:48:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const targetUrl = 'https://openebs-community.slack.com/join/shared_invite/enQtMjQzMTg4NTcyNTY2LTJiMzVjYjA5ZDk3YmI4NjAxY2QyYmI3MTA1MmUxMTAzNTU0NTM5NTViOTIxMjA1NWQ4NzVmMTBiNjk0NDU1YzQ';
 
+
+//call back function
 function handleRedirect(req, res) {
     res.redirect(targetUrl);
 }
@@ -11,6 +13,7 @@ app.get('*', (req, res) => {
     res.send('Not Found!')
 })
 
+//change the value here if you're using a different port num
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(` Server is running on port ${PORT}`);
