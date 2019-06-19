9bfd8bfc21406ad191c273eae9b41b2d501a268e
Amrish Kushwaha <askmaurya48@gmail.com>
2019-06-19 20:36:40 +0530

    Update slack redirection link
    Signed-off-by: Amrish Kushwaha <askmaurya48@gmail.com>
    This commit will update new slack redirection link.

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const targetUrl = 'https://openebs-community.slack.com/join/shared_invite/enQtMjQzMTg4NTcyNTY2LTJiMzVjYjA5ZDk3YmI4NjAxY2QyYmI3MTA1MmUxMTAzNTU0NTM5NTViOTIxMjA1NWQ4NzVmMTBiNjk0NDU1YzQ';
+const targetUrl = 'https://openebs-community.slack.com/join/shared_invite/enQtNjcyMDI4NTc4MzQyLTA5MWYyZDQ3ZmE0MWU0YjU0ZDM5ZjlmNjNiNzhjMDNjMjk4MmQ0MDM3OGRmODczZDVhYjlmMzYzZWI2ZWYxNGQ';
 
 
 //call back function
@@ -9,10 +9,9 @@ function handleRedirect(req, res) {
 }
 
 app.get("/", handleRedirect);
-app.get('*', (req, res) =>  res.send('Not Found!'));
+app.get('*', (req, res) => res.send('Not Found!'));
 
 
 //change the value here if you're using a different port num
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(` Server is running on port ${PORT}`));
-
